82c68878afb27cb42a7ed0b95556904ae1b9df58
Krzysztof Platek <krzysztofplatek@poczta.onet.pl>
2026-03-21 07:43:32 +0000

    wip

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -6,6 +6,9 @@ docker compose -f docker/docker-compose.yml up -d --build jupyter
 
 echo "Spark cluster & Jupyter started"
 
+docker exec -it minio mc alias set local http://localhost:9000 minio minio123
+docker exec -it minio mc mb local/warehouse
+
 sleep 5
 
 docker ps
