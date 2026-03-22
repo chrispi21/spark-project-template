529bda8d835eb7f5092e99b3758816e98768c7b3
Krzysztof Platek <krzysztofplatek@poczta.onet.pl>
2026-03-22 20:31:36 +0000

    s3 working

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -2,13 +2,15 @@
 
 echo "Starting Spark cluster & Jupyter..."
 
-docker compose -f docker/docker-compose.yml up -d --build jupyter
+docker compose -f docker/docker-compose.yml up -d --build
 
 echo "Spark cluster & Jupyter started"
+
+sleep 5
 
 docker exec -it minio mc alias set local http://localhost:9000 minio minio123
 docker exec -it minio mc mb local/warehouse
 
-sleep 5
+echo "'warehouse' bucket created"
 
 docker ps
