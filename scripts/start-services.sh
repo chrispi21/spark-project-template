512f6f01e398ce189918d97aff5ab2e46ffa98a5
Krzysztof Platek <kplatek.external@epo.org>
2026-03-26 13:23:43 +0000

    fixes

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -18,8 +18,8 @@ echo "Spark cluster & Jupyter started"
 
 sleep 5
 
-docker exec -it minio mc alias set local http://localhost:9000 minio minio123
-docker exec -it minio mc mb local/warehouse
+docker exec minio /usr/bin/mc alias set local http://localhost:9000 minio minio123
+docker exec minio /usr/bin/mc mb --ignore-existing local/warehouse
 
 echo "'warehouse' bucket created"
 
